62087bb52704153c45596aed41bd2851f3dca4a4
sdhaduk <sagardhaduk1@gmail.com>
2023-12-23 18:45:20 -0600

    created navbar and added to layout

---
+++ src/app/NavBar.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Link from "next/link";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { usePathname } from "next/navigation";
+
+export default function NavBar() {
+  const pathname = usePathname();
+
+  return (
+    <Navbar
+      bg="primary"
+      variant="dark"
+      sticky="top"
+      expand="sm"
+      collapseOnSelect
+    >
+      <Container>
+        <Navbar.Brand as={Link} href="/">
+          NextJS Image Gallery
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="main-navbar" />
+        <Navbar.Collapse id="main-navbar">
+          <Nav>
+            <Nav.Link as={Link} href="/hello" active={pathname === "/hello"}>
+              Hello
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Container, SSRProvider } from "@/components/bootstrap";
+import { Container } from "@/components/bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
+import NavBar from "./NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NavBar />
         <main>
           <Container className="py-4">{children}</Container>
         </main>
