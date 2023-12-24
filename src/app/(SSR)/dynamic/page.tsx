ca0666859c31f088ddc56f5631f19f64e1c7710b
sdhaduk <sagardhaduk1@gmail.com>
2023-12-23 21:45:00 -0600

    fixed typo in route

--- src/app/(SSR)/dynamic/page.tsx
+++ src/app/(SSR)/dynamic/page.tsx
@@ -35,7 +35,7 @@ export default async function Page() {
         className="rounded shadow mw-100 h-100"
       />
       by{" "}
-      <Link href={`/users/${image.user.username}`}>{image.user.username}</Link>
+      <Link href={`/user/${image.user.username}`}>{image.user.username}</Link>
     </div>
   );
 }
